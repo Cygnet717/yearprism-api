1dc8bce3b71f0a42ba2ce0598b5626c45159d625
Kathy Bradt <kathybradt717@gmail.com>
2021-04-12 14:24:02 -0500

    production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://katge@localhost/yearprism',
     JWT_EXPIRY: process.env.JWT_EXPIRY || "24h",
     JWT_SECRET: process.env.JWT_SECRET || 'my-own-special-jwt-secret',
